d2008076e57858911b64bbe961d6fcc8d7e2bded
er-omsonar <er.omsonar@gmail.com>
2025-09-15 17:19:31 +0530

    build a component

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -1,9 +1,26 @@
 import { useState } from 'react'
+import useCurrencyInfo from './hooks/useCurrencyinfo'
 import './App.css'
+import { InputBox } from './components'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount,setAmount] = useState(0)
+  const [from,setFrom] = useState("usd")
+  const [to,setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+  const currencyInfo = useCurrencyInfo(from)
+  const options = Object.keys(currencyInfo)
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
 
+  }
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to])
+  }
   return (
     <>
       <h1 className='text-3xl bg-amber-700'>currency app with chai</h1>
